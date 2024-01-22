3d550a85fa6afb1d5304c9e06a364eeca56594b6
Gloria <ubahgloria16@gmail.com>
2024-01-22 11:16:20 +0100

    Folder setup

---
+++ portfoliov2/app/data.tsx
@@ -0,0 +1,75 @@
+import Img1 from "./public/Screenshot_20230423-103100_Brave.webp";
+import Img2 from "./public/Screenshot_20230423-102849_Brave.webp";
+import Img3 from "./public/Screenshot_20230423-102834_Brave.webp";
+import Img4 from "./public/Screenshot_20230423-102801_Brave.webp";
+
+
+const Projects = [
+     {
+          id: 0,
+          title: "T&Cs Generator",
+          img:Img1,
+          description: "A web application that generates T&Cs and Privacy Policy in a few clicks. Powerful, self-serve product with different templates to suit diverse products, businesses, contracts, profiles, mobile apps, third party services and many more.",
+          url: "https://zuri-training.github.io/t_c-generator-team71/index.html",
+          github: "https://github.com/daisy28"
+     },
+     {
+          id: 1,
+          title: "TempKit",
+          img:Img2,
+          description: "A weather app that checks for and displays the current weather condition based on location. It accesses users location to generate and display precise weather information. UI info changes based on latitude, longitude, weather icons, date and time based on time zones.",
+          url: "https://daisy28.github.io/TempKit/",
+          github: "https://github.com/daisy28"
+     },
+     {
+          id: 2,
+          title: "Speak Better",
+          img:Img3,
+          description: "Speak better is an AI-powered grammar checker designed to check and correct grammar for multiple languages. Backed by an automatic speech recognition system trained with over 650,000 hours of multilingual speech data collected from all over the internet A brainchild of Carpenter-team-grit’s mission of having a population of excellent grammarians. It offers the necessary tools for fluent and effective communication.",
+          url: "https://speakbetter.fun/",
+          github: "https://github.com/daisy28"
+     },
+     {
+          id: 3,
+          title: "AgroLearn",
+          img:Img4,
+          description: "We are equipping the next generation of Agrarian leaders in Africa through online learning. Gain a competitive edge with our courses that meet global standards. Our curriculum is developed in partnership with leading institutions around the world, giving you the knowledge and expertise to succeed in the global market.",
+          url: "https://agrolearn.org/",
+          github: "https://github.com/daisy28"
+     },
+
+      {
+          id: 4,
+          title: "Modelling Agency",
+          img:Img1,
+          description: "We are the No. 1 Modelling Agency in the Country, we discover talents, mould and expose them to the world. Experience a whole different modelling experiences with top coaches and awesome partners.",
+          url: "https://daisy28.github.io/Models",
+          github: "https://github.com/daisy28"
+     },
+     {
+          id: 5,
+          title: "Metabnb",
+          img:Img2,
+          description: "Rent a place away from Home in the Metaverse. We provide access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone.",
+          url: "https://daisy28.github.io/Metabnb/",
+          github: "https://github.com/daisy28"
+     },
+     {
+          id: 6,
+          title: "Mavin",
+          img:Img3,
+          description: "Mavins taste for delivering the utmost value, has inspired the company's expression into innovative services such as partnerships, content production, publishing and marketing. We encourage prospective artistes to keep pushing out their sample covers till they get their big break.",
+          url: "https://daisy28.github.io/mavins",
+          github: "https://github.com/daisy28"
+     },
+     {
+          id: 7,
+          title: "Art Agency",
+          img:Img4,
+          description: "The perfect Artworks for your Homes and Offices. Explore creativity at its peak. Go through the beautiful art works we have in store and experience purity, innocence, expressed through art by our various talented artists.",
+          url: "https://daisy28.github.io/Art-agency",
+          github: "https://github.com/daisy28"
+     },
+]
+
+export default Projects;
---
+++ portfoliov2/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Home = () => {
+  return(
+    <p className={`text-2xl text-red-500 underline`}>Hello world</p>
+  )
+}
+
+export default Home;
